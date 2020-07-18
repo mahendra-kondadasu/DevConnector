1a23b9a70e2c61cbeef2def354456173d9614fc7
MahendraKondadasu <mahendrakondadasu@gmail.com>
2020-07-18 22:28:51 +0530

    feat: implement Request and Body validation for Registration route. To validate the request and body parameters we use include middleware. express-validatormodule is used to validate the parameters. we can use check() method to validate the input parameters for different conditions. If the parameters are not valid we will return erros with 400 status code.

--- server.js
+++ server.js
@@ -7,6 +7,10 @@ const app = express();
 
 connectDB();
 
+//Init Middleware
+
+app.use(express.json({ extended: false }));
+
 app.get("/", (req, res) => res.send("API Running.."));
 
 app.use('/api/users', require('./routes/api/users'));
