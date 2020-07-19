0af6abebe9f56cad0c2b213c495882d81e46e96d
MahendraKondadasu <mahendrakondadasu@gmail.com>
2020-07-19 14:32:12 +0530

    feat: Implement user registration

--- config/db.js
+++ config/db.js
@@ -1,19 +1,25 @@
 const mongoose = require("mongoose");
 const config = require("config");
-const db = config.get("mongoURI");
+const dotenv = require("dotenv");
+dotenv.config();
+
+const db = process.env.mongoURI;
 
 const connectDB = async () => {
-    try {
-        await mongoose.connect(db, { useUnifiedTopology: true, useNewUrlParser: true });
+  try {
+    await mongoose.connect(db, {
+      useUnifiedTopology: true,
+      useNewUrlParser: true,
+      useCreateIndex: true,
+    });
 
-        console.log("MongoDB Connected...");
-    }
-    catch (err) {
-        console.err(err.message);
+    console.log("MongoDB Connected...");
+  } catch (err) {
+    console.error(err.message);
 
-        //Exit process with failure
-        process.exit(1);
-    }
-}
+    //Exit process with failure
+    process.exit(1);
+  }
+};
 
 module.exports = connectDB;
