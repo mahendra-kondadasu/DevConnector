0933f97234d222e998035c052bc9bc5aac5eafe2
MahendraKondadasu <mahendrakondadasu@gmail.com>
2020-07-19 20:19:13 +0530

    feat: get current loggedin user profile route implemented a route to get the current logged in user profile details

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -1,10 +1,27 @@
 const express = require("express");
 const router = express.Router();
+const auth = require("../../middleware/auth");
+const Profile = require("../../models/Profile");
+const User = require("../../models/User");
+const mongoose = require("mongoose");
 
-// @route       GET /api/profile
-// @desc        Test route
-// @access      Public
+// @route       GET /api/profile/me
+// @desc        Get current users profile
+// @access      Private
 
-router.get('/', (req, res) => res.send('Profile route'));
+router.get("/me", auth, async (req, res) => {
+  try {
+    const profile = await Profile.findOne({
+      user: req.user.id,
+    }).populate("user", ["name", "avatar"]);
+    if (!profile) {
+      return res.status(400).json({ msg: "There is no Profile for this user" });
+    }
+    res.json(profile);
+  } catch (err) {
+    console.error(err);
+    res.status(500).send("Server Error");
+  }
+});
 
 module.exports = router;
