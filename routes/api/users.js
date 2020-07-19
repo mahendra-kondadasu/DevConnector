1c1313fc7762f9a72681b87a424560a31b2cf65a
MahendraKondadasu <mahendrakondadasu@gmail.com>
2020-07-19 16:22:28 +0530

    feat: Implement JWT

--- routes/api/users.js
+++ routes/api/users.js
@@ -2,6 +2,7 @@ const express = require("express");
 const router = express.Router();
 const gravatar = require("gravatar");
 const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
 
 const { check, validationResult } = require("express-validator");
 
@@ -63,7 +64,23 @@ router.post(
 
       //Return jsonWebToken
 
-      res.send("User registered");
+      const payload = {
+        user: {
+          id: user.id,
+        },
+      };
+
+      jwt.sign(
+        payload,
+        process.env.jwtSecret,
+        {
+          expiresIn: 360000,
+        },
+        (err, token) => {
+          if (err) throw err;
+          res.json({ token });
+        }
+      );
     } catch (err) {
       console.error(err.message);
       res.status(500).send("Server error");
